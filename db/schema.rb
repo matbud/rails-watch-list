7023d27670c574f8d9acd2a5281b40df23306338
Mateusz Budzisz <mat.budzisz@gmail.com>
2021-08-19 11:30:41 +0200

    added validations to models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_085310) do
+ActiveRecord::Schema.define(version: 2021_08_19_090431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_08_19_085310) do
     t.string "poster_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "rating"
   end
 
   add_foreign_key "bookmarks", "lists"
