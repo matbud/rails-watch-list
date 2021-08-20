c37f1468020b444ee7d915ddbf58487e597a018f
Mateusz Budzisz <mat.budzisz@gmail.com>
2021-08-20 12:27:09 +0200

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,8 @@
 require 'open-uri'
 require 'json'
 
-# url = "http://tmdb.lewagon.com/movie/top_rated"
-# movies = JSON.parse(URI.open(url).read)
-# movies['results'].each do |movie|
-#   Movie.create!(title: movie['title'], overview: movie["overview"], rating: movie["vote_average"], poster_url: "https://image.tmdb.org/t/p/w500" + movie["poster_path"])
-# end
+url = "http://tmdb.lewagon.com/movie/top_rated"
+movies = JSON.parse(URI.open(url).read)
+movies['results'].each do |movie|
+  Movie.create!(title: movie['title'], overview: movie["overview"], rating: movie["vote_average"], poster_url: "https://image.tmdb.org/t/p/w500" + movie["poster_path"])
+end
